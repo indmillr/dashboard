19f81e07c25c4d9f9aca5911c3a54cbb33c29d99
Ian Miller <theiandmiller@gmail.com>
2022-06-25 05:43:35 -0500

    Routes boilerplate

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const activeMenu = false;
 
   return (
     <div>
@@ -31,17 +31,42 @@ const App = () => {
             <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar w-0</div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? "md:ml-72" : "flex-2"
             }`}
           >
-            <div className='fixed mg-static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               Navbar
             </div>
-          </div>
 
-          <div>
-            <Routes></Routes>
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path='/' element='ECommerce' />
+                <Route path='/ecommerce' element='ECommerce' />
+
+                {/* Pages */}
+                <Route path='/orders' element='Orders' />
+                <Route path='/employees' element='Employees' />
+                <Route path='/customers' element='Customers' />
+
+                {/* Apps */}
+                <Route path='/kanban' element='Kanban' />
+                <Route path='/editor' element='Editor' />
+                <Route path='/calendar' element='Calendar' />
+                <Route path='/color-picker' element='ColorPicker' />
+
+                {/* Charts */}
+                <Route path='/line' element='Line' />
+                <Route path='/area' element='Area' />
+                <Route path='/bar' element='Bar' />
+                <Route path='/pie' element='Pie' />
+                <Route path='/financial' element='Financial' />
+                <Route path='/color-mapping' element='ColorMapping' />
+                <Route path='/pyramid' element='Pyramid' />
+                <Route path='/stacked' element='Stacked' />
+              </Routes>
+            </div>
           </div>
         </div>
       </BrowserRouter>
